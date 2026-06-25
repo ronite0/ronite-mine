9e51ea238e25c60b8680d33faaf13ea3579dc818
Ronite <bitzerodotbase@gmail.com>
2026-06-25 16:26:58 +0700

    Update vite-env.d.ts

--- ronitev1/src/vite-env.d.ts
+++ ronitev1/src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_BACKEND_URL: string;
   readonly VITE_RONITE_TOKEN_ADDRESS: string;
+  // Staking contracts
   readonly VITE_COAL_STAKING_ADDRESS: string;
   readonly VITE_COAL_REWARD_TOKEN_ADDRESS: string;
   readonly VITE_IRON_STAKING_ADDRESS: string;
@@ -11,6 +12,13 @@ interface ImportMetaEnv {
   readonly VITE_GOLD_REWARD_TOKEN_ADDRESS: string;
   readonly VITE_DIAMOND_STAKING_ADDRESS: string;
   readonly VITE_DIAMOND_REWARD_TOKEN_ADDRESS: string;
+  // Ore market contracts
+  readonly VITE_COAL_MARKET_ADDRESS: string;
+  readonly VITE_IRON_MARKET_ADDRESS: string;
+  readonly VITE_GOLD_MARKET_ADDRESS: string;
+  readonly VITE_DIAMOND_MARKET_ADDRESS: string;
+  // Airdrop Season 1
+  readonly VITE_AIRDROP_CONTRACT_ADDRESS: string;
 }
 
 interface ImportMeta {
